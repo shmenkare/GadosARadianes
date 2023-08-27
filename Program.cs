026173c4d77270d453af1b266fcef8a597f615cd
“shmenkare” <shmenkare@gmail.com>
2023-08-27 16:16:39 -0400

    grados a radianes 2.0

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace GadosARadianes
     {
         static void Main(string[] args)
         {
-            //Grados a Radianes 1.0
+            //Grados a Radianes 2.0
             /*Una revolución completa es igual a 2? rad
             (o) 360°. 
             1° = 0,017453 radianes y 1 rad = 57,2958°. 
@@ -18,14 +18,37 @@ namespace GadosARadianes
 
             double grados = 1.0;
             double radian = 1.0;
+            int opcion = 0;
 
-            Console.WriteLine("Introduce los grados a convertir");
-            grados= Convert.ToDouble(Console.ReadLine());
 
-            radian = grados*0.017453;
+            Console.WriteLine("Que operacion desea realizar");
+            Console.WriteLine("1- grados a radianes");
+            Console.WriteLine("2- radianes a grados");
+            opcion = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("{0} grados es igual a {1} radianes", grados, radian);
+            if (opcion == 1)
+            {
+                Console.WriteLine("Introduce los grados a convertir");
+                grados = Convert.ToInt32(Console.ReadLine());
 
+                radian = grados * 0.017453;
+
+                Console.WriteLine("{0} grados es igual a {1} radianes", grados, radian);
+            }
+            else if (opcion == 2)
+            {
+                Console.WriteLine("Introduce los radianes a convertir");
+                radian = Convert.ToInt32(Console.ReadLine());
+                grados = radian * 57.2958;
+
+                Console.WriteLine("{0} radianes es igual a {1} grados", radian, grados);
+            }
+            else 
+            {
+                Console.WriteLine("Opcion no valida");
+
+            }
+            
             Console.ReadKey();
 
 
